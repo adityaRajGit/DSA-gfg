5b068c1162bb470bcade66a21aa99d1263e57a1e
Aditya Raj <72968234+adityaRajGit@users.noreply.github.com>
2023-05-14 23:44:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add two numbers represented by linked lists/add-two-numbers-represented-by-linked-lists.cpp
@@ -0,0 +1,127 @@
+//{ Driver Code Starts
+// driver
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+// } Driver Code Ends
+/* node for linked list:
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    private:
+     struct Node *reverse(struct Node *head){
+        Node *prev=NULL;
+        Node *curr=head;
+        Node *forward=NULL;
+        while(curr!=NULL){
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
+    public:
+    //Function to add two numbers represented by linked list.
+    struct Node* addTwoLists(struct Node* first, struct Node* second)
+    {
+        // code here
+        Node *dummy=new Node(-1);
+        Node *point=dummy;
+        first=reverse(first);
+        second=reverse(second);
+        int carry=0;
+        while(first!=NULL || second!=NULL || carry){
+            int sum=0;
+            if(first!=NULL){
+                sum += first->data;
+                first=first->next;
+            }
+            if(second!=NULL){
+                sum += second->data;
+                second=second->next;
+            }
+            sum += carry;
+            carry=sum/10;
+            Node *newNode=new Node(sum%10);
+            point->next=newNode;
+            point=point->next;
+        }
+       return reverse(dummy->next);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution ob;
+        Node* res = ob.addTwoLists(first,second);
+        printList(res);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
